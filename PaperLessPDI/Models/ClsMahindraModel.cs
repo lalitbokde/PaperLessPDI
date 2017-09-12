c7f37c806a264e69aad3a49b8d4cfbe7524f9868
Dhiraj Bhalme <dhirajbhalme15@gmail.com>
2017-09-12 14:01:18 +0530

    Changes Done

--- PaperLessPDI/Models/ClsMahindraModel.cs
+++ PaperLessPDI/Models/ClsMahindraModel.cs
@@ -6,7 +6,7 @@ namespace PaperLessPDI.Models
 {
     public static class StatusModel
     {
-        public static string Url = "http://192.168.0.3:8080/APIForMobile/";
+        public static string Url = "http://192.168.1.36:8070/APIForMobile/";
         //public static string Url = "http://outletdealer.suvarnapp.com/APIForMobile/";
         public static int UserTokenNo;
         public static int UserID;
